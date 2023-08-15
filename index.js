d2da100c6bc16f212c35bf30cc5015e3cce46f18
JOSHUA-MUANLAL <105428544+JOSHUA-MUANLAL@users.noreply.github.com>
2023-08-15 17:47:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function skill(){
+
+    document.getElementById("skills").style.display='block';
+    document.getElementById("educations").style.display='none';
+  }
+  function education(){
+
+    document.getElementById("skills").style.display='none';
+    document.getElementById("educations").style.display='block';
+  }
